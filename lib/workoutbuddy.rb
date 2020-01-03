498c2ed44610011d996dd4af18d1b4e8cf18e2e9
Sean Welsh Brown <seanwelshbrown@gmail.com>
2020-01-03 11:25:54 -0500

    All user account info options added

--- lib/workoutbuddy.rb
+++ lib/workoutbuddy.rb
@@ -1,4 +1,5 @@
 require "tty-prompt"
+require "tty-table"
 require "pry"
 
 PROMPT = TTY::Prompt.new
@@ -11,7 +12,6 @@ class WorkoutBuddy
 #*** RUNNER METHOD ***#
 
   def run 
-    greet
     login
     sleep 2
     main_menu
@@ -19,26 +19,29 @@ class WorkoutBuddy
 
 #~~~ GENERAL METHODS ~~~#
 
-  def greet
-    system 'clear'
-    puts "Welcome to WorkoutBuddy, your source for quality exercise tips and your very own personal workout tracker!"
-    puts "\n"
+  def logo 
+    file = File.open("lib/ascii/logo.txt")
+    logo = file.read
+    puts logo 
   end
 
-  def exit_app
+  def exit_app(current_menu)
+    system 'clear'
+
     input = PROMPT.select('Are you sure you want to exit?', ["Yes", "No"], cycle: true)
     if input == "Yes"
       puts "So long for now!"
       abort
     elsif input == "No"
-      system 'clear'
-      go_to_previous_menu(1)
+      current_menu.to_sym
+      send(current_menu)
     end
   end
 
-  def exit_check(input)
-    if input == "exit"
-      exit_app
+  def back_check(input, current_menu)
+    if input == "go back"
+      current_menu.to_sym
+      send(current_menu)
     end
   end
 
@@ -46,44 +49,50 @@ class WorkoutBuddy
     User.find_by(username: input)
   end
 
-  def go_to_previous_menu(caller_num) # MAKE BLOG POST ABOUT THIS #
-    previous_method = caller[caller_num].split("`").pop.gsub("'", "")
-    previous_method.to_sym
-
-    # previous_method = caller[1].label
-    # previous_method.to_sym
-    send(previous_method)
-  end
+  # def go_to_previous_menu(caller_num) # MAKE BLOG POST ABOUT THIS #
+  #   previous_method = caller[caller_num].split("`").pop.gsub("'", "")
+  #   previous_method.to_sym
+  #   send(previous_method)
+  # end
 
 #~~~ LOGIN METHODS ~~~#
 
   def login
-    input = PROMPT.select("Please log in to your existing account, or create a new account if you are a new user. (It's easy, we promise!)", ["Log in", "Create an Account", "Exit Application"], cycle: true)
+    system 'clear'
+
+    puts logo
+
+    puts "Welcome to WorkoutBuddy, your source for quality exercise tips and your very own personal exercise tracker!"
+    puts "\n"
+
+    input = PROMPT.select("Please log in to your existing account, or create a new account if you are a new user. (It's easy, we promise!)", ["Log in", "Create an Account", "\u{1f6ab} Exit Application"], cycle: true)
 
     if input == "Log in"
       log_into_account
     elsif input == "Create an Account"
       puts "Thank you for choosing to create an account!"
       create_account
-    elsif input == "Exit Application"
-      exit_app
+    elsif input == "\u{1f6ab} Exit Application"
+      exit_app("login")
     end
   end
 
   def log_into_account
     system 'clear'
-    input = PROMPT.ask("Please enter your Username, or 'create account' to create a new account. You may also enter 'exit' to exit the program.", convert: :string, required: true)
+    input = PROMPT.ask("Please enter your Username, or 'create account' to create a new account. You may also enter 'go back' to return to the previous menu.", convert: :string, required: true)
     if input == "create account"
       create_account
     end
-    exit_check(input)
+    back_check(input, "login")
 
     user_check = find_user_by_username(input)
     if user_check
+      system 'clear'
       @@current_user = user_check
       puts "Successfully logged in. Nice to see you again, #{@@current_user.first_name}!"
     elsif !user_check
       puts "No User Account found with that Username. Please try again."
+      sleep 1.5
       log_into_account
     end
   end
@@ -96,13 +105,14 @@ class WorkoutBuddy
     last_name = nil
     age = nil
 
-    input1 = PROMPT.ask("Please enter a Username. You may use only upper and lower case letters, as well as numbers. You may also enter 'exit' to exit the program.", convert: :string, required: true)
+    input1 = PROMPT.ask("Please enter a Username. You may use only upper and lower case letters, as well as numbers. You may also enter 'go back' to return to the previous menu:", convert: :string, required: true)
+    
+    back_check(input1, "login")
 
     if input1.index( /[^[:alnum:]]/ ) || input1.include?(" ")
       puts "Username contains invalid characters."
       create_account
     end
-    exit_check(input1)
 
     user_check = find_user_by_username(input1)
 
@@ -111,17 +121,18 @@ class WorkoutBuddy
       create_account
     elsif !user_check
       username = input1
-      first_name = PROMPT.ask("Please enter your first name:", default: nil, convert: :string, required: true)
-      exit_check(first_name)
-      last_name = PROMPT.ask("Please enter your last name:", default: nil, convert: :string, required: true)
-      exit_check(last_name)
-      age = PROMPT.ask("Please enter your current age:", default: nil, convert: :int, required: true)
-      exit_check(age)
+      first_name = PROMPT.ask("Please enter your first name. You may also enter 'go back' to return to the previous menu:", default: nil, convert: :string, required: true)
+      back_check(first_name, "login")
+      last_name = PROMPT.ask("Please enter your last name. You may also enter 'go back' to return to the previous menu:", default: nil, convert: :string, required: true)
+      back_check(last_name, "login")
+      age = PROMPT.ask("Please enter your current age. You may also enter 'go back' to return to the previous menu:", default: nil, convert: :int, required: true)
+      back_check(age, "login")
       
       sleep 1.5
 
       @@current_user = User.create(username: username, first_name: first_name, last_name: last_name, age: age)
       puts "Account successfully created! You are now logged in as #{username}."
+      return
     end
 
   end
@@ -134,20 +145,20 @@ class WorkoutBuddy
     puts "*--- MAIN MENU ---*"
     puts "\n"
 
-    input = PROMPT.select("Please select an option to navigate, #{@@current_user.first_name}:", ["View Exercises", "View/Edit Your Saved Exercises", "Edit Account Information", "Log Out (#{@@current_user.username})", "Exit Application"], cycle: true)
+    input = PROMPT.select("Please select an option to navigate, #{@@current_user.first_name}:", ["View Exercises", "View/Edit Your Saved Exercises", "Edit Account Information", "\u{1f6b7} Log Out (#{@@current_user.username})", "\u{1f6ab} Exit Application"], cycle: true)
 
     if input == "View Exercises"
       view_exercises
     elsif input == "View/Edit Your Saved Exercises"
       view_edit_saved_exercises
     elsif input == "Edit Account Information"
-
-    elsif input == "Log Out (#{@@current_user.username})"
+      account_info
+    elsif input == "\u{1f6b7} Log Out (#{@@current_user.username})"
       @@current_user = nil
       system 'clear'
       run
-    elsif input == "Exit Application"
-      exit_app
+    elsif input == "\u{1f6ab} Exit Application"
+      exit_app("main_menu")
     end
   end
 
@@ -260,11 +271,11 @@ class WorkoutBuddy
     puts "*--- VIEW EXERCISES ---*"
     puts "\n"
 
-    input = PROMPT.select("Would you like to learn about Strength Training or Cardio exercises?", ["Strength Training", "Cardio", "\u{2b05}  Go back to previous menu"], cycle: true)
+    input = PROMPT.select("Would you like to learn about Strength Training or Cardio exercises?", ["\u{1F3CB} Strength Training", "\u{1F3C3} Cardio", "\u{2b05}  Go back to previous menu"], cycle: true)
 
-    if input == "Strength Training"
+    if input == "\u{1F3CB} Strength Training"
       strength_training
-    elsif input == "Cardio"
+    elsif input == "\u{1F3C3} Cardio"
       cardio
     elsif input == "\u{2b05}  Go back to previous menu"
       main_menu
@@ -279,13 +290,13 @@ class WorkoutBuddy
     puts "*--- STRENGTH TRAINING ---*"
     puts "\n"
 
-    input = PROMPT.select("What part of the body would you like to focus on?", ["Upper Body", "Legs", "Core", "\u{2b05}  Go back to previous menu"], cycle: true)
+    input = PROMPT.select("What part of the body would you like to focus on?", ["\u{1F4AA} Upper Body", "\u{1F9B5} Legs", "\u{1F455} Core", "\u{2b05}  Go back to previous menu"], cycle: true)
 
-    if input == "Upper Body"
+    if input == "\u{1F4AA} Upper Body"
       upper_body
-    elsif input == "Legs"
+    elsif input == "\u{1F9B5} Legs"
       legs
-    elsif input == "Core"
+    elsif input == "\u{1F455} Core"
       core
     elsif input == "\u{2b05}  Go back to previous menu"
       view_exercises
@@ -418,6 +429,137 @@ class WorkoutBuddy
     end
   end
 
-#~~~ EDIT USER ACCOUNT INFORMATION ~~~#
+#~~~ USER ACCOUNT INFORMATION ~~~#
+
+  def account_info
+    system 'clear'
+    table = TTY::Table.new [['Username:', @@current_user.username], ['First Name:', @@current_user.first_name], ['Last Name:', @@current_user.last_name], ['Age', @@current_user.age]]
+
+    # table = TTY::Table[['a1', 'a2'], ['b1', 'b2']]
+
+    puts "*--- ACCOUNT INFO ---*"
+    puts "\n"
+    puts "CURRENT INFORMATION"
+    puts "\n"
+    puts table.render :unicode
+
+    puts "\n"
+
+    input = PROMPT.select("Would you like to change any of your account information?", ["Username", "First Name", "Last Name", "Age", "\u{1f6ab}  Delete account", "\u{2b05}  Go back to previous menu"], cycle: true)
+
+    if input == "Username"
+      change_username
+    elsif input == "First Name"
+      change_first_name
+    elsif input == "Last Name"
+      change_last_name
+    elsif input == "Age"
+      change_age
+    elsif input == "\u{1f6ab}  Delete account"
+      system 'clear'
+      input2 = PROMPT.select("Are you sure you want to delete your account? This is a permanent action, there will be no recovery of data possible after confirmation.", ["Yes", "No"])
+      if input2 == "Yes"
+        delete_account
+      elsif input2 == "No"
+        account_info
+      end
+    elsif input == "\u{2b05}  Go back to previous menu"
+      main_menu
+    end
+  end
+
+    #--- ACCOUNT METHODS ---*
+
+  def change_username
+    system 'clear'
+
+    input = PROMPT.ask("Please enter a new Username. You may use only upper and lower case letters, as well as numbers. You may also enter 'go back' to return to the previous menu.", convert: :string, required: true)
+
+    back_check(input, "account_info")
+
+    if input.index( /[^[:alnum:]]/ ) || input.include?(" ")
+      puts "Username contains invalid characters, please try again."
+      sleep 1.5
+      change_username
+    end
+
+    user_check = find_user_by_username(input)
+
+    if user_check
+      puts "This Username already exists. Please try again."
+      sleep 1.5
+      change_username
+    elsif !user_check 
+      @@current_user.username = input
+      @@current_user.save
+      sleep 1.5
+      input2 = PROMPT.select("Username successfully changed!", "\u{2b05}  Go back to previous menu")
+      if input2
+        account_info
+      end
+    end
+  end
+
+  def change_first_name
+    system 'clear'
+
+    input = PROMPT.ask("Please enter a new preferred First Name. You may also enter 'go back' to return to the previous menu:", convert: :string, required: true)
+
+    back_check(input, "account_info")
+
+    sleep 1.5
+    @@current_user.first_name = input
+    @@current_user.save
+
+    input2 = PROMPT.select("First Name successfully changed. Hello #{@@current_user.first_name}!", "\u{2b05}  Go back to previous menu")
+    if input2
+      account_info
+    end
+  end
+
+  def change_last_name
+    system 'clear'
+
+    input = PROMPT.ask("Please enter a new Last Name. You may also enter 'go back' to return to the previous menu:", convert: :string, required: true)
+
+    back_check(input, "account_info")
+
+    sleep 1.5
+    @@current_user.last_name = input
+    @@current_user.save
+
+    input2 = PROMPT.select("Last Name successfully changed. #{@@current_user.first_name} #{@@current_user.last_name} it is!", "\u{2b05}  Go back to previous menu")
+    if input2
+      account_info
+    end
+  end
+
+  def change_age
+    system 'clear'
+
+    input = PROMPT.ask("Please update your age. You may also enter 'go back' to return to the previous menu:", convert: :string, required: true)
+
+    back_check(input, "account_info")
+
+    input.to_int
+
+    sleep 1.5
+    @@current_user.age = input
+    @@current_user.save
+
+    input2 = PROMPT.select("Age successfully updated. #{@@current_user.age} looks good on you!", "\u{2b05}  Go back to previous menu")
+    if input2
+      account_info
+    end
+  end
+
+  def delete_account
+    sleep 1
+    User.find_by(id: @@current_user.id).destroy
+    input = PROMPT.select("Account successfully deleted, we're sorry to see you go.", ["\u{2b05}  Return to Login menu"])
+    if input
+      login
+    end
+  end
 
 end
